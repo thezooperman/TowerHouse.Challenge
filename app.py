b2d0f2ab63e98227584b3789a2a3657ae417ab91
Aritra Ghosh <arghos@microsoft.com>
2018-01-21 11:46:45 +0530

    now fetches current currency info from external source

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from db_client import *
 import requests, json
 
 app = Flask(__name__)
+# cache = Cache(config={'CACHE_TYPE':'simple'})
+# cache.init_app(app)
 CORS(app)
 
 
 @app.route('/', methods=['GET'])
+# @cache.cached(timeout=300, key_prefix="currency_index")
 def get_index():
     """Handles GET requests for index page"""
     response_list = []
     btc_result = r.db(PROJECT_DB).table(PROJECT_TABLE_BTC). \
-        order_by(index=r.desc(PRIMARY_KEY)).limit(1).pluck('priceUSD', 'date', 'txVolumeUSD').run(db_connection)
+        order_by(index=r.desc(PRIMARY_KEY)).limit(1).pluck(
+            'priceUSD', 'date', 'txVolumeUSD').run(db_connection)
     btc_result.items[0]['currency'] = 'Bitcoin'
     eth_result = r.db(PROJECT_DB).table(PROJECT_TABLE_ETHERIUM). \
         order_by(index=r.desc(PRIMARY_KEY)).limit(1).run(db_connection)
@@ -31,13 +35,16 @@ def get_index():
 
 
 @app.route('/api/currency/<string:currency>', methods=['GET'])
+# @cache.cached(timeout=300, key_prefix="currency_history")
 def get_history(currency):
     """GET requests for historical price and volume info for selected currency"""
     _currency = return_currency_code(currency)
-    _tab = r.db(PROJECT_DB).table(_currency).order_by(index=r.desc('date')).pluck('date', 'priceUSD', 'txVolumeUSD')
+    _tab = r.db(PROJECT_DB).table(_currency).order_by(
+        index=r.desc('date')).pluck('date', 'priceUSD', 'txVolumeUSD')
     query = _tab.run(db_connection)
     if query is not None:
-        print('query returned %d rows for currency - %s' % (len(query.items), currency))
+        print('query returned %d rows for currency - %s' %
+              (len(query.items), currency))
     else:
         print('query returned no rows for currency - %s', currency)
     return_val = [q for q in query.items]
@@ -48,14 +55,20 @@ def get_history(currency):
 def get_current_price(currency):
     """Gets the current price for selected currency from external source"""
     _currency = return_currency_code(currency)
-    print('currency - ', _currency)
     key = 'SQ53PLSDSYK58K0A'
     url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_' \
           'INTRADAY&symbol={}&market=USD&apikey={}'.format(_currency, key)
-    print('URL - ', url)
-    r = requests.get(url)
-    print('Request Status - %d, Reason - %s' % (r.status_code, r.reason))
-    return jsonify({})
+    response = {}
+    with requests.get(url, stream=True) as r:
+        print('Currency - %s, URL - %s, Request Status - %d, Reason - %s' %\
+            (_currency, url, r.status_code, r.reason))
+        data = r.json()
+        my_data = data['Time Series (Digital Currency Intraday)']
+        for k,v in my_data.items():
+            vals = list(v.values())
+            response = {'date': k, 'price': vals[0],'volume': vals[2]}
+            break
+    return jsonify(response)
 
 
 def return_currency_code(currency):
--- html/scripts/controllers.js
+++ html/scripts/controllers.js
@@ -1,5 +1,5 @@
 angular.module('myApp.Controllers', [])
-//IndexController
+    //IndexController
     .controller('IndexController', function ($scope, $http, $localStorage, $window) {
         $scope.currencies = [];
         $http.get('http://localhost:9090/').then(function (response) {
@@ -21,15 +21,56 @@ angular.module('myApp.Controllers', [])
         $http.get('http://localhost:9090/api/currency/' + _currency).then(function (response) {
             $scope.historicalData = response.data;
             delete $localStorage.SelectedCurrency;
-            $scope.refreshDate = $scope.historicalData[0].date
-            $scope.refreshPrice = $scope.historicalData[0].priceUSD
-            $scope.refreshVolume = $scope.historicalData[0].txVolumeUSD
+            $scope.refreshDate = '--';//$scope.historicalData[0].date;
+            $scope.refreshPrice = '--';//$scope.historicalData[0].priceUSD;
+            $scope.refreshVolume = '--';//$scope.historicalData[0].txVolumeUSD;
         });
+        // switch (_currency) {
+        //     case 'DogeCoin':
+        //         _currency = 'doge'
+        //         break;
+        //     case 'Ethereum':
+        //         _currency = 'eth'
+        //         break;
+        //     case 'Litecoin':
+        //         _currency = 'ltc'
+        //         break;
+        //     default:
+        //         _currency = 'btc';
+        // }
+        var url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=' + _currency + '&market=USD&apikey=SQ53PLSDSYK58K0A';
 
-        $interval(function() {
-           $scope.refreshPrice = $http.get('http://localhost:9090/api/getprice/' + _currency)
-               .then(function (response) {
-                  var a = response.data;
-               });
+        // var interval = $interval(function () {
+        //     $http.get(url)
+        //         .then(function (response) {
+        //             var newArr = [];
+        //             angular.forEach(response.data, (msg)=>
+        //             {
+        //                 // if (msg.hasOwnProperty('Time Series (Digital Currency Intraday)'))
+        //                 // {
+        //                     // console.log(msg);
+        //                     // console.log(response.data[msg]);
+        //                     if (msg !== undefined || msg !== null) {
+        //                         newArr.push(msg);
+        //                         // angular.forEach(msg, (text)=>{
+        //                         //     console.log(text);
+        //                         // });
+        //                     }
+        //                 // }
+        //             });
+        //             console.log(newArr);
+        //             $scope.refreshDate = response.data.date;
+        //             $scope.refreshPrice = response.data.price;
+        //             $scope.refreshVolume = response.data.volume;
+        //         });
+        // }, 5000);
+
+        var interval = $interval(function () {
+            $http.get('http://localhost:9090/api/getprice/' + _currency)
+                .then(function (response) {
+                    $scope.refreshDate = response.data.date;
+                    $scope.refreshPrice = response.data.price;
+                    $scope.refreshVolume = response.data.volume;
+                });
         }, 2000);
     });
